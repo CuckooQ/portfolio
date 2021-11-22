b33d96e48c28bb913e68bc6f8350ad2eb2e0bccb
jaeyong4536 <jaeyong4536@gmail.com>
2021-11-22 21:19:10 +0900

    푸터 추가

---
+++ src/components/sections/Footer.jsx
@@ -0,0 +1,16 @@
+import "../../styles/Footer.scss";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="left">Portfolio &copy; 2021</div>
+      <div className="line"></div>
+      <div className="right">
+        <span>Created by</span>
+        <span>JAEYONG CHO</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
