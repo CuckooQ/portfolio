6366341708400fa65e64a8183d901634520591ab
jaeyong4536 <jaeyong4536@gmail.com>
2021-11-29 16:11:26 +0900

    버그 수정

--- src/components/Wavey.jsx
+++ src/components/Wavey.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import gsap from "gsap";
 import Wave from "./Wave";
+import { DEVICE, getDevice } from "../utils/device";
 import "../styles/Wavey.scss";
 
 function Wavey(props) {
@@ -35,52 +36,62 @@ function Wavey(props) {
     };
     gsap.to(backText1Ref.current, {
       ...config,
+      x: `${figure * 1.5}${unit}`,
+      y: `${figure * 1.5}${unit}`,
+    });
+    gsap.to(frontText1Ref.current, {
+      ...config,
       x: `${figure}${unit}`,
       y: `${figure}${unit}`,
     });
-    gsap.to(frontText1Ref.current, config);
     gsap.to(backText2Ref.current, {
       ...config,
-      x: `${figure * -20}${unit}`,
-      y: `${figure}${unit}`,
+      x: `${figure * -1}${unit}`,
+      y: `${figure * 1.5}${unit}`,
     });
     gsap.to(frontText2Ref.current, {
       ...config,
-      x: `${figure * -21}${unit}`,
+      x: `${figure * -1.5}${unit}`,
+      y: `${figure}${unit}`,
     });
     gsap.to(backText3Ref.current, {
       ...config,
-      x: `${figure * -1}${unit}`,
-      y: `${figure}${unit}`,
+      x: `${figure * 1.5}${unit}`,
+      y: `${figure * 1.5}${unit}`,
     });
     gsap.to(frontText3Ref.current, {
       ...config,
-      x: `${figure * -2}${unit}`,
+      x: `${figure}${unit}`,
+      y: `${figure}${unit}`,
     });
   }, [figure, config]);
 
   return (
-    <section className="wavey-wrapper">
+    <section className="wavey-wrapper" id="home">
       <div className="text-wrapper">
         <h2 className="back-name" ref={backText1Ref}>
           JAEYONG CHO
         </h2>
         <h2 className="back-name" ref={backText2Ref}>
-          FRONTEND DEVELOPER
+          FRONT-END DEVELOPER
         </h2>
         <h2 className="back-name" ref={backText3Ref}>
           PASSIONATE TO DEVELOPMENT
         </h2>
       </div>
 
-      <Wave className="wavey-scene" />
+      {getDevice() === DEVICE.TABLET ? (
+        <img className="wavey-scene" src="/images/wavey.jpg" alt="Wave"></img>
+      ) : (
+        <Wave className="wavey-scene" />
+      )}
 
       <div className="text-wrapper">
         <h2 className="front-name" ref={frontText1Ref}>
           JAEYONG CHO
         </h2>
         <h2 className="front-name" ref={frontText2Ref}>
-          FRONTEND DEVELOPER
+          FRONT-END DEVELOPER
         </h2>
         <h2 className="front-name" ref={frontText3Ref}>
           PASSIONATE TO DEVELOPMENT
---
+++ src/utils/device.js
@@ -0,0 +1,20 @@
+export const DEVICE = {
+  TABLET: "tablet",
+  MOBILE: "mobile",
+  DESKTOP: "desktop",
+};
+
+export function getDevice() {
+  const userAgent = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(userAgent)) {
+    return DEVICE.TABLET;
+  }
+  if (
+    /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      userAgent
+    )
+  ) {
+    return DEVICE.MOBILE;
+  }
+  return DEVICE.DESKTOP;
+}
